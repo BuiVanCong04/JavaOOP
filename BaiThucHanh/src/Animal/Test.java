43a09947957a0db3af5b96ed82a0e4fae8826f93
BuiVanCong04 <buicong24112004@gmail.com>
2024-05-09 22:08:16 +0700

    bai thuc hanh

---
+++ BaiThucHanh/src/Animal/Test.java
@@ -0,0 +1,14 @@
+package Animal;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal cat = new Cat();
+        cat.move();
+        cat.eat();
+        cat.ngancach();
+        Aminal dog = new Dog();
+        dog.move();
+        dog.eat();
+        dog.ngancach();
+    }
+}
